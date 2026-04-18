3d89485236454efe4bbd735809c05e5346722777
Iker-Araujo-Dominguez <you@example.com>
2026-04-18 12:57:37 +0200

    KonexioForm-eko estruktura hasiera

--- KonexioForm.cs
+++ KonexioForm.cs
@@ -4,5 +4,24 @@ namespace Pizzeria
 {
     public class KonexioForm : Form
     {
+        public string Zerbitzaria { get; private set; }
+        public string DatuBasea { get; private set; }
+        public string Erabiltzailea { get; private set; }
+        public string Pasahitza { get; private set; }
+
+        public KonexioForm()
+        {
+            Zerbitzaria = "localhost";
+            DatuBasea = "pizzeria";
+            Erabiltzailea = "root";
+            Pasahitza = "1MG32025";
+        }
+
+        public void ErroreaErakutsi(string mezua)
+        {
+            MessageBox.Show(mezua, "Errorea",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
     }
 }
