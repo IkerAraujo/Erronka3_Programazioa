6173deaf22a6056b4eca663da11977f53f86a1f5
Iker-Araujo-Dominguez <you@example.com>
2026-04-18 12:50:57 +0200

    Datu base lehenengo estruktura

--- DatuBasea.cs
+++ DatuBasea.cs
@@ -2,5 +2,26 @@ namespace Pizzeria
 {
     public static class DatuBasea
     {
+        private static string zerbitzaria;
+        private static string datuBasea;
+        private static string erabiltzailea;
+        private static string pasahitza;
+
+        public static void KonexioaEzarri(
+            string zerbitzariaBerria,
+            string datuBaseaBerria,
+            string erabiltzaileaBerria,
+            string pasahitzaBerria)
+        {
+            zerbitzaria = zerbitzariaBerria;
+            datuBasea = datuBaseaBerria;
+            erabiltzailea = erabiltzaileaBerria;
+            pasahitza = pasahitzaBerria;
+        }
+
+        public static bool KonexioaEgiaztatu()
+        {
+            return true;
+        }
     }
 }
